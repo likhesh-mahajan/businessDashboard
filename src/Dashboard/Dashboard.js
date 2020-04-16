095e3922584de732c053d1b3384f8a4c5f56b87f
Likhesh Mahajan <likhesh@Mac-Pro.local>
2020-04-16 16:33:30 +1000

    Business Dashboard

---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,26 @@
+import React, { useState, createContext } from 'react';
+import { Brand } from './Brand';
+import { Container } from './Container';
+import { GlobalStyle } from './GlobalStyle';
+import { Chart } from './Chart';
+import { VirtualizedTable } from './Table';
+import { DarkSwitch } from './DarkSwitch';
+
+export const ThemeContext = createContext();
+
+export function Dashboard() {
+  const themeState = useState('light');
+  const [theme, setTheme] = themeState;
+  const dark = theme === 'dark';
+  return (
+    <ThemeContext.Provider value={themeState}>
+      <GlobalStyle dark={dark} />
+      <Container>
+        <Brand />
+        <DarkSwitch />
+        <Chart />
+        <VirtualizedTable />
+      </Container>
+    </ThemeContext.Provider>
+  );
+}
